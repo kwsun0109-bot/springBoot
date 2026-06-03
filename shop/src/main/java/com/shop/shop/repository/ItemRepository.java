e04a067a6a12bc34f2898d1d96277ba33cc20b97
kwsun0109@gmail.com <kwsun70>
2026-06-03 16:57:29 +0900

    first uploading...

---
+++ shop/src/main/java/com/shop/shop/repository/ItemRepository.java
@@ -0,0 +1,28 @@
+package com.shop.shop.repository;
+// CRUD 및 페이징처리
+
+import com.shop.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor; /* 인터페이스 */
+
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> { // 제네릭타입 엔티티타입 클래스, 기본키 타입
+
+    List<Item> findByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value="select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
+}
